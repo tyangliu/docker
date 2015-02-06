9224210672784f317797f87f36836683fb6402f9
Saied Kazemi <saied@google.com>
2015-02-05 20:32:27 -0800

    Checkpoint/Restore Support: add exec driver methods
    Methods for checkpointing and restoring containers were added to the
    native driver.  The LXC driver returns an error message that these
    methods are not implemented yet.
    
    Signed-off-by: Saied Kazemi <saied@google.com>
    
    Conflicts:
    	daemon/execdriver/native/create.go
    	daemon/execdriver/native/driver.go
    	daemon/execdriver/native/init.go
    
    Conflicts:
    	daemon/execdriver/driver.go
    	daemon/execdriver/native/create.go

--- daemon/execdriver/driver.go
+++ daemon/execdriver/driver.go
@@ -39,6 +39,8 @@ type Hooks struct {
 	PostStop []DriverCallback
 }
 
+type RestoreCallback func(*ProcessConfig, int)
+
 // Info is driver specific information based on
 // processes registered with the driver
 type Info interface {
@@ -80,6 +82,10 @@ type Driver interface {
 
 	// Unpause unpauses a container.
 	Unpause(c *Command) error
+
+	Checkpoint(c *Command) error
+
+	Restore(c *Command, pipes *Pipes, restoreCallback RestoreCallback) (int, error)
 
 	// Name returns the name of the driver.
 	Name() string
