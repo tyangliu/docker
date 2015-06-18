59e575c6af1d01af4346067b1e39e299f62fe925
boucher <rboucher@gmail.com>
2015-06-18 15:18:09 -0700

    Don't destroy/delete the container if it has been checkpointed.
    Docker-DCO-1.1-Signed-off-by: Ross Boucher <rboucher@gmail.com> (github: boucher)

--- daemon/execdriver/native/driver.go
+++ daemon/execdriver/native/driver.go
@@ -158,8 +158,11 @@ func (d *Driver) Run(c *execdriver.Command, pipes *execdriver.Pipes, startCallba
 	d.activeContainers[c.ID] = cont
 	d.Unlock()
 	defer func() {
-		cont.Destroy()
-		d.cleanContainer(c.ID)
+		status, err := cont.Status()
+		if err != nil || status != libcontainer.Checkpointed {
+			cont.Destroy()
+			d.cleanContainer(c.ID)
+		}
 	}()
 
 	if err := cont.Start(p); err != nil {
